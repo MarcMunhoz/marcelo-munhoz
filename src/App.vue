f4a52d8c079d9bd4c6f560adee004d3f1d392cf6
Marcelo Munhoz <me@marcelomunhoz.com>
2023-05-16 17:40:29 -0300

    Starting SEO setup

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,23 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useMeta } from "quasar";
+
+const metaData = {
+  titleTemplate: (title) => `Marcelo Munhoz Website - ${title}`,
+
+  meta: {
+    author: { name: "author", content: "Marcelo Munhoz <me@marcelomunhoz.com>" },
+    description: { name: "description", content: "Some brief histories of my past-present development experience. The life, the universe and everything about a tech life" },
+    keywords: { name: "keywords", content: "blog,personal,pessoal,developer,Quasar,Vue,Vuejs,coding,webtech,tips,dicas" },
+    equiv: { "http-equiv": "Content-Type", content: "text/html; charset=UTF-8" },
+  },
+};
 
 export default defineComponent({
   name: "App",
+  setup() {
+    useMeta(metaData);
+  },
 });
 </script>
