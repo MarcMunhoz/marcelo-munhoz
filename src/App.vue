275c6ccd4b3870d92e81592a127d9f240bfd0c8e
Marcelo Munhoz <me@marcelomunhoz.com>
2023-06-17 23:14:39 -0300

    Privacy policy added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <router-view :key="$route.fullPath" />
+
+  <q-dialog v-model="alert" persistent full-width position="bottom" transition-hide="fade" class="text-lg">
+    <q-card class="">
+      <q-card-section class="">Olá! O site utiliza o Google Analytics para coletar informações sobre as visitas e interações dos usuáriosm tais como: endereço IP, localização geográfica, fonte de referência, tipo de navegador, duração da visita e páginas visitadas. Esses dados nos ajudam a entender o desempenho do site, aprimorar sua funcionalidade e fornecer uma melhor experiência para você. Ao continuar navegando, você está consentindo com a associação desses dados coletados pelo Google Analytics às informações que coletamos. </q-card-section>
+
+      <q-card-actions align="center">
+        <q-btn flat label="OK" color="grey-5" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { createMetaMixin } from "quasar";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    return {
+      alert: ref(true),
+      address: ref(""),
+    };
+  },
   mixins: [
     createMetaMixin(function () {
       return {
