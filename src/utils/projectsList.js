4ab70b13a7e750125885d5de30b6046d833d6656
Marcelo Munhoz <me@marcelomunhoz.com>
2023-06-23 11:36:28 -0300

    New mini project added | Tooltip for projects list added

--- src/utils/projectsList.js
+++ src/utils/projectsList.js
@@ -1,30 +1,40 @@
-// ICONS LIST https://raw.githubusercontent.com/saqy/vuejs-emojis/master/src/emoji.json
+// ICONS SOURCE => https://fonts.google.com/icons
 
 const projecstList = [
   {
     projectName: "Weather App",
     projectUrl: "https://munhoz-weather-app.herokuapp.com",
     projectEmoji: "thunderstorm",
+    projectTooltip: "Simple NodeJS weather web app"
   },
   {
     projectName: "Random Pass",
     projectUrl: "https://random-pass.marcelomunhoz.com",
     projectEmoji: "lock",
+    projectTooltip: "Web app that generates a random string every 5 seconds. With 'Copy to Clipboard' button"
   },
   {
     projectName: "Public Flickr Photos",
     projectUrl: "https://flickr-gallery.marcelomunhoz.com",
     projectEmoji: "photo_camera",
+    projectTooltip: "Website that shows all public photos of the desired profile"
   },
   {
     projectName: "Vuetify Todo",
     projectUrl: "https://vuetify-todo.marcelomunhoz.com",
     projectEmoji: "checklist_rtl",
+    projectTooltip: "Simple 'ToDo' web app"
   },
   {
     projectName: "365 Movies",
     projectUrl: "https://365movies.marcelomunhoz.com",
     projectEmoji: "movie",
+  },
+  {
+    projectName: "Tiny Image App",
+    projectUrl: "https://github.com/MarcMunhoz/tiny-img-app",
+    projectEmoji: "compress",
+    projectTooltip: "NodeJS app that reduces the file size of your images"
   },
 ];
 
