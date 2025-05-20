f29b4b109f941f8eb60b0e0de0c1c6d7221663b8
Marcelo Munhoz <me@marcelomunhoz.com>
2025-05-20 17:04:51 -0300

    fix: dist folder path and Dockerfile minor bugs

--- app/middleware/server.js
+++ app/middleware/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import contentfulRoutes from "./routes/contentful.js";
+
+dotenv.config();
 
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
+// Keeps banckend alive
+app.get("/healthz", (_, res) => {
+  res.status(200).send("OK");
+});
+
 // 👉 Serve frontend if a build exists (production)
-const distPath = path.resolve(__dirname, "../dist");
+const distPath = path.resolve(__dirname, "../dist/spa");
 app.use(express.static(distPath));
 
 // 👉 API
